8ec1a0f5f8fbda72add141d0c3dd98fbcb652b6d
MarcLohfink <77617650+MarcLohfink@users.noreply.github.com>
2023-12-08 09:04:54 +0100

    Update runserverlocally.bat

--- runserverlocally.bat
+++ runserverlocally.bat
@@ -1,2 +1,16 @@
-cd ./docfx_project
+@echo off
+echo Start local docfx server
+
+setlocal
+
+set "currentDir=%cd%"
+
+echo Process in %~dp0\docfx_project/
+cd %~dp0/docfx_project/
+
+echo run docfx docfx.json --serve
 docfx docfx.json --serve
+
+cd %currentDir%
+
+endlocal
